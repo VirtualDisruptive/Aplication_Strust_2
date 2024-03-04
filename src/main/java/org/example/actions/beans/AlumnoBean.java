8ff4af381d9d206a3be4d0d2f6fe3047e43ffdeb
Estiben Ramirez <stevendisruptive@gmail.com>
2024-03-04 19:34:15 +0100

    Added bean

---
+++ src/main/java/org/example/actions/beans/AlumnoBean.java
@@ -0,0 +1,43 @@
+package org.example.actions.beans;
+
+
+public class AlumnoBean {
+
+    private String name;
+    private String lastName;
+    private String dni;
+    private String phone;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
